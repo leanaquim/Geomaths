88180bae667b289adac0426cd41bc038b7f5c79f
Leana Quimerc'H <quimerch1u@etu.univ-lorraine.fr>
2022-12-15 17:19:57 +0100

    faults - first try

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import poisson
 
 from chevron_attributes import horizon_id_chevron
 from ifp1_attributes import horizon_id_ifp1
-from ifp2_attributes import horizon_id_ifp2
+from ifp2_attributes import horizon_id_ifp2, is_fault_ifp2
 from shell_attributes import horizon_id_shell
 
 
@@ -27,14 +27,69 @@ def extract_horizons(mesh, horizon_id):
     return horizon_list
 
 
-def main(model, mesh, horizon_id):
+def extract_faults(mesh, is_fault):
+    ncorn = mesh.ncorners
+    nb_faults = count_nb_faults(mesh, is_fault)
+    fault_list = [[] for _ in range(nb_faults)]
+    for fault in range(nb_faults):
+        for vertex in range(ncorn):
+            if is_fault[vertex]:
+                fault_list[fault].append(vertex)
+    return fault_list
+
+
+# def count_nb_faults(mesh, is_fault):
+#     nb_faults = 0
+#     dict = {}
+#     ncorn = mesh.ncorners
+#     for i in range(ncorn):
+#         if is_fault[i]:
+#             if nb_faults not in dict.keys():
+#                 dict[nb_faults] = []
+#                 nb_faults += 1
+#             if mesh.dst(i) in dict[nb_faults]:
+#                 dict[nb_faults].append(mesh.org(i))
+#                 dict[nb_faults].append(mesh.dst(i))
+#     return False
+
+
+def rec_count(mesh, halfedge, nb_faults, is_fault, tag):
+    next_halfedge = mesh.opposite(mesh.prev(mesh.opposite(halfedge)))
+    if is_fault[next_halfedge]:
+        tag[next_halfedge] = nb_faults
+        rec_count(mesh, next_halfedge, nb_faults, tag)
+    else :
+        tag[next_halfedge] = -1
+
+
+def count_nb_faults(mesh, is_fault):
+    nb_faults = 0
+    ncorn = mesh.ncorners
+    tag = np.zeros(ncorn)
+    for halfedge in range(ncorn):
+        if tag[halfedge] == 0:
+            if is_fault[halfedge]:
+                nb_faults += 1
+                tag[halfedge] = nb_faults
+                rec_count(mesh, halfedge, nb_faults, is_fault, tag)
+            else :
+                tag[halfedge] = -1
+    return nb_faults
+
+
+def flatten(model, mesh, horizon_id):
     horizon_list = extract_horizons(mesh, horizon_id)
     poisson.flatten_horizons(model, mesh, horizon_list)
 
 
 if __name__ == '__main__' :
     
-    main('chevron', Mesh("chevron/slice.obj"), horizon_id_chevron)
-    main('ifp1', Mesh("ifp1/slice.obj"), horizon_id_ifp1)
-    main('ifp2', Mesh("ifp2/slice.obj"), horizon_id_ifp2)
-    main('shell', Mesh("shell/slice.obj"), horizon_id_shell)
+    # flatten('chevron', Mesh("chevron/slice.obj"), horizon_id_chevron)
+    # flatten('ifp1', Mesh("ifp1/slice.obj"), horizon_id_ifp1)
+    # flatten('ifp2', Mesh("ifp2/slice.obj"), horizon_id_ifp2)
+    # flatten('shell', Mesh("shell/slice.obj"), horizon_id_shell)
+
+    #print(extract_faults(Mesh("ifp2/slice.obj"), is_fault_ifp2))
+    fault_list = extract_faults(Mesh("ifp2/slice.obj"), is_fault_ifp2)
+    print([len(fault_list[i]) for i in range(len(fault_list))])
+    # Probleme : 216 fois la meme liste...
